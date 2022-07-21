11941e73063ca413d1891b1e9c96b543e455a660
Scott LeFoll <51521131+scottlefoll@users.noreply.github.com>
2022-07-20 20:43:36 -0500

    update

--- SpaceWar/main.py
+++ SpaceWar/main.py
@@ -6,7 +6,7 @@ pygame.font.init()
 
 WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Space Invaders")
+pygame.display.set_caption("SpaceWar")
 
 # Load images
 RED_SPACE_SHIP = pygame.image.load("spacewar/assets/pixel_ship_red_small2.png")
